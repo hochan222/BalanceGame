c92353501468cc9bf2a4902c48d1daea1a812063
judy5050 <68409061+judy5050@users.noreply.github.com>
2022-02-24 00:23:05 +0900

    Feature/#29 entity setting (#40)
    * feat: :sparkles: application.yaml 설정 #29
    
    * feat: :sparkles: entity setting #29

---
+++ backend/src/main/java/com/example/backend/domain/Article.java
@@ -0,0 +1,60 @@
+package com.example.backend.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Article extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_category_id")
+    private ArticleCategory articleCategory;
+
+    @Column(length = 45, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TINYTEXT", nullable = false)
+    private String content;
+
+    @Column(length = 100, nullable = false)
+    private String leftItem;
+
+    @Column(length = 100, nullable = false)
+    private String rightItem;
+
+    @Column(nullable = false)
+    private Long leftCount;
+
+    @Column(nullable = false)
+    private Long rightCount;
+
+    @Column(nullable = false)
+    private Long totalCount;
+
+    @Column(length = 8, nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
+    private Set<ArticleComment> articleComments = new HashSet<>();
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
+    private Set<VoteHistory> voteHistories = new HashSet<>();
+}
---
+++ backend/src/main/java/com/example/backend/domain/VoteHistory.java
@@ -0,0 +1,31 @@
+package com.example.backend.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class VoteHistory extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 8, nullable = false)
+    private String password;
+
+    @Column(length = 300, nullable = false)
+    private String userId;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
