19fad565c61d126b015cbd24efc41d8718dd84d7
hochan222 <hochan049@gmail.com>
2022-02-25 10:59:40 +0900

    feat: ✨ 게시글 링크 기능 추가

--- frontend/src/components/History/History.tsx
+++ frontend/src/components/History/History.tsx
@@ -5,16 +5,15 @@ import ArticleModel from '@/models/ArticleModel';
 import { convertTimeToLeftTime } from '@/utils/utils';
 
 const History = ({ article }: { article: ArticleModel }) => {
-  const { title, content, createdAt, voteCount } = article;
+  const { title, content, createdAt, voteCount, id } = article;
   const { leftTime, unit } = convertTimeToLeftTime(createdAt);
 
-  // TODO: 해당 링크 연결하기
-  // const moveArticlePage = (): void => {
-  //   document.location.href = `/article?articleId=${id}`;
-  // }
+  const moveArticlePage = (): void => {
+    document.location.href = `/article?articleId=${id}`;
+  };
 
   return (
-    <section className="history">
+    <section className="history" onClick={moveArticlePage} onKeyDown={moveArticlePage} role="button" tabIndex={0}>
       <h3>{title}</h3>
       <p className="history__content">{content}</p>
       <div className="history__footer">
