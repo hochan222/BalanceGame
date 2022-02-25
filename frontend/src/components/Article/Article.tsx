ec14d4abf89a559ea083058e8730dcae361c1e78
hochan222 <hochan049@gmail.com>
2022-02-25 11:27:14 +0900

    fix: 🐛 vote 동기화

--- frontend/src/components/Article/Article.tsx
+++ frontend/src/components/Article/Article.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import {
-  Container, VStack, Text, Grid,
+  Container,
+  VStack,
+  Text,
+  Grid,
   GridItem,
   Textarea,
   Button,
   Flex,
   IconButton,
-  useDisclosure
+  useDisclosure,
 } from '@chakra-ui/react';
 
-import {ArrowLeftIcon, ArrowRightIcon, EditIcon, CheckIcon } from '@chakra-ui/icons';
+import { ArrowLeftIcon, ArrowRightIcon, EditIcon, CheckIcon } from '@chakra-ui/icons';
 import { observer } from 'mobx-react-lite';
 import AriticleStore from '@/stores/ArticleStore';
 import UIStore from '@/stores/UIStore';
@@ -20,56 +23,56 @@ interface IArticleProp {
   uiStore: UIStore;
 }
 
-const Article = ({ store, uiStore } : IArticleProp) => {
+const Article = ({ store, uiStore }: IArticleProp) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { article,  patchVote } = store;
+  const { article, patchVote } = store;
   const { selectedArticleId } = uiStore;
   const [voted, setVoted] = useState<boolean>(false);
-  const [selectedVote, selectVote] = useState<string>("");
+  const [leftCount, setLeftCount] = useState<number>(0);
+  const [rightCount, setRightCount] = useState<number>(0);
+  const [selectedVote, selectVote] = useState<string>('');
 
   const enterOtherArticle = (offset: number): void => {
-    if(selectedArticleId + offset < 1) {
-      alert("마지막 질문 입니다.");
-    }else {
+    if (selectedArticleId + offset < 1) {
+      alert('마지막 질문 입니다.');
+    } else {
       document.location.href = `/article?articleId=${selectedArticleId + offset}`;
     }
   };
 
-  const vote = (clickedVote: string) : void => {
-      if(!voted){
-        setVoted(true);
-        selectVote(clickedVote);
-        patchVote(clickedVote);
-      }
-  }
+  const vote = (clickedVote: string): void => {
+    if (!voted) {
+      setVoted(true);
+      selectVote(clickedVote);
+      patchVote(clickedVote);
+    }
+  };
 
   return (
     <>
       <VStack w="100%">
         <Container marginTop={5} marginBottom={3} pr={0}>
           <Flex justify="center" align="center">
-              <Text fontSize='lg' align="center" fontWeight="700">Q. {article.title}</Text>
-              <IconButton
-                variant="unstyled"
-                aria-label='wow'
-                borderRadius="none"
-                p={0} ml={2} mr={0}
-                icon={<EditIcon/>}
-                onClick={onOpen}
-              />
-            </Flex>
+            <Text fontSize="lg" align="center" fontWeight="700">
+              Q. {article.title}
+            </Text>
+            <IconButton
+              variant="unstyled"
+              aria-label="wow"
+              borderRadius="none"
+              p={0}
+              ml={2}
+              mr={0}
+              icon={<EditIcon />}
+              onClick={onOpen}
+            />
+          </Flex>
         </Container>
-        <Grid
-          templateColumns="1fr 1fr"
-          gap={1}
-          h="max-content"
-          minH={180}
-          w="100%"
-        >
+        <Grid templateColumns="1fr 1fr" gap={1} h="max-content" minH={180} w="100%">
           <GridItem overflowX="auto">
             <Container
-              bg={voted ? "gray.200": "red.100"}
-              as={voted ? "div": "button"}
+              bg={voted ? 'gray.200' : 'red.100'}
+              as={voted ? 'div' : 'button'}
               paddingTop={2}
               paddingBottom={2}
               borderRadius="md"
@@ -78,31 +81,32 @@ const Article = ({ store, uiStore } : IArticleProp) => {
               h="full"
               display="flex"
               alignItems="center"
-              onClick={()=>vote("left")}
+              onClick={() => {
+                vote('left');
+                setLeftCount(1);
+              }}
             >
               <VStack w="full">
-                {
-                  article.leftItem?.split("\n").map((text) => {
-                    return <Text fontSize="md" color="red.500" w="full" align="center">{text}</Text>
-                  })
-                }
-                {
-                  voted && (
-                    <Text fontSize="2xl" color="red.500" w="full" align="center">{article.leftCount}표</Text>
-                  )
-                }
-                {
-                  selectedVote === "left" && (
-                    <CheckIcon color='green.500'/>
-                  )
-                }
+                {article.leftItem?.split('\n').map((text) => {
+                  return (
+                    <Text fontSize="md" color="red.500" w="full" align="center">
+                      {text}
+                    </Text>
+                  );
+                })}
+                {voted && (
+                  <Text fontSize="2xl" color="red.500" w="full" align="center">
+                    {article.leftCount + leftCount <= 0 ? 0 : article.leftCount + leftCount}표
+                  </Text>
+                )}
+                {selectedVote === 'left' && <CheckIcon color="green.500" />}
               </VStack>
             </Container>
           </GridItem>
           <GridItem overflowX="auto">
             <Container
-              bg={voted ? "gray.200": "blue.100"}
-              as={voted ? "div": "button"}
+              bg={voted ? 'gray.200' : 'blue.100'}
+              as={voted ? 'div' : 'button'}
               paddingTop={2}
               paddingBottom={2}
               borderRadius="md"
@@ -110,58 +114,63 @@ const Article = ({ store, uiStore } : IArticleProp) => {
               display="flex"
               alignItems="center"
               justifyContent="center"
-              onClick={()=>vote("right")}
+              onClick={() => {
+                vote('right');
+                setRightCount(1);
+              }}
             >
               <VStack w="full">
-                {
-                  article.rightItem?.split("\n").map((text) => {
-                    return <Text fontSize="md" color="blue.500" w="full" align="center">{text}</Text>
-                  })
-                }
-                {
-                  voted && (
-                    <Text fontSize="2xl" color="blue.500" w="full" align="center">{article.rightCount}표</Text>
-                  )
-                }
-                {
-                  selectedVote === "right" && (
-                    <CheckIcon color='green.500'/>
-                  )
-                }
+                {article.rightItem?.split('\n').map((text) => {
+                  return (
+                    <Text fontSize="md" color="blue.500" w="full" align="center">
+                      {text}
+                    </Text>
+                  );
+                })}
+                {voted && (
+                  <Text fontSize="2xl" color="blue.500" w="full" align="center">
+                    {article.rightCount + rightCount <= 0 ? 0 : article.rightCount + rightCount}표
+                  </Text>
+                )}
+                {selectedVote === 'right' && <CheckIcon color="green.500" />}
               </VStack>
             </Container>
           </GridItem>
         </Grid>
         <Container bg="gray.300">
-          <Textarea 
-            disabled 
-            h="max-content" 
-            size="sm" 
-            variant="unstyled" 
-            value={article.content as string} 
-            borderRadius="sm" 
+          <Textarea
+            disabled
+            h="max-content"
+            size="sm"
+            variant="unstyled"
+            value={article.content as string}
+            borderRadius="sm"
           />
         </Container>
       </VStack>
       <Flex justify="space-between" marginX={4} marginTop={1}>
-        <Button 
-          size="xs" 
-          variant="ghost" 
-          bg="lavender" 
+        <Button
+          size="xs"
+          variant="ghost"
+          bg="lavender"
           leftIcon={<ArrowLeftIcon />}
-          onClick={()=>enterOtherArticle(-1)}
-        >이전 질문</Button>
-        <Button 
-          size="xs" 
-          variant="ghost" 
-          bg="lavender" 
+          onClick={() => enterOtherArticle(-1)}
+        >
+          이전 질문
+        </Button>
+        <Button
+          size="xs"
+          variant="ghost"
+          bg="lavender"
           rightIcon={<ArrowRightIcon />}
-          onClick={()=>enterOtherArticle(1)}
-        >다음 질문</Button>
+          onClick={() => enterOtherArticle(1)}
+        >
+          다음 질문
+        </Button>
       </Flex>
-      <ArticleModal isOpen={isOpen} onClose={onClose} selectedArticleId={selectedArticleId}/>
+      <ArticleModal isOpen={isOpen} onClose={onClose} selectedArticleId={selectedArticleId} />
     </>
   );
-}
+};
 
 export default observer<IArticleProp>(Article);
--- frontend/src/components/EditModal/EditModal.tsx
+++ frontend/src/components/EditModal/EditModal.tsx
@@ -32,54 +32,48 @@ export interface EditFormInput {
 export const EditModal = ({ onClose, isOpen }: UseDisclosureProps) => {
   const rootStore: RootStore = useContext(StoreContext) as RootStore;
   const { editStore } = rootStore;
-  const {  postArticle } = editStore;
+  const { postArticle } = editStore;
 
   const modalClose = () => {
     if (onClose) {
       onClose();
     }
-  }
+  };
 
   const { register, handleSubmit, setValue, clearErrors, setError, watch, formState } = useForm<EditFormInput>();
 
   // submit시에 password를 확인하여 에러를 반환하는 함수 구현
-  const validatePassword = useCallback(
-    () => {
-      const password = watch("password", "");
-      if (password === undefined || password.length < 4) {
-        setError("password", {
-          type: "min"
-        })
-      } else {
-        clearErrors("password");
-      }
+  const validatePassword = useCallback(() => {
+    const password = watch('password', '');
+    if (password === undefined || password.length < 4) {
+      setError('password', {
+        type: 'min',
+      });
+    } else {
+      clearErrors('password');
     }
-    , [watch, setError, clearErrors]
-  );
+  }, [watch, setError, clearErrors]);
 
   // submit시에 password를 확인하여 에러를 반환하는 함수 구현
-  const validateCategory = useCallback(
-    () => {
-      const categories = watch("category");
-      if (categories === undefined || categories.length <= 0) {
-        setError("category", {
-          type: "required"
-        })
-      } else {
-        clearErrors("category");
-      }
+  const validateCategory = useCallback(() => {
+    const categories = watch('category');
+    if (categories === undefined || categories.length <= 0) {
+      setError('category', {
+        type: 'required',
+      });
+    } else {
+      clearErrors('category');
     }
-    , [watch, setError, clearErrors]
-  );
+  }, [watch, setError, clearErrors]);
 
-  const onSubmit: SubmitHandler<EditFormInput> = data => {
+  const onSubmit: SubmitHandler<EditFormInput> = (data) => {
     validateCategory();
     validatePassword();
     postArticle(data);
   };
 
   return (
-    <Modal onClose={modalClose} isOpen={isOpen || false} size="xl" >
+    <Modal onClose={modalClose} isOpen={isOpen || false} size="xl">
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>밸런스 게임 질문 등록</ModalHeader>
@@ -89,21 +83,21 @@ export const EditModal = ({ onClose, isOpen }: UseDisclosureProps) => {
             <Grid gap={7}>
               <Grid gap={3}>
                 <FormControl marginBottom={3} isRequired>
-                  <FormLabel htmlFor='title'>밸런스 게임 질문 (상황 설명)</FormLabel>
+                  <FormLabel htmlFor="title">밸런스 게임 질문 (상황 설명)</FormLabel>
                   {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-                  <Input {...register("title")} variant="filled" placeholder='질문을 입력해주세요.' size="sm" />
+                  <Input {...register('title')} variant="filled" placeholder="질문을 입력해주세요." size="sm" />
                 </FormControl>
                 <EditorContentSection register={register} />
               </Grid>
               <EditorCategoriesSection formState={formState} clearErrors={clearErrors} setValue={setValue} />
               <EditorPasswordSection formState={formState} clearErrors={clearErrors} setValue={setValue} />
-              <Button colorScheme='twitter' marginBottom={3} type="submit">
+              <Button colorScheme="twitter" marginBottom={3} type="submit">
                 등록하기
               </Button>
             </Grid>
           </form>
-        </ModalBody >
-      </ModalContent >
-    </Modal >
-  )
-}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
